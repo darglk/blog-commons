4c4041e1c7655a3292967ec4f0ccebc628c77b46
darglk <darglk@gmail.com>
2023-03-11 14:21:26 +0100

    refactor validation exception

--- src/main/java/com/darglk/blogcommons/exception/ValidationException.java
+++ src/main/java/com/darglk/blogcommons/exception/ValidationException.java
@@ -7,16 +7,22 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ValidationException extends CustomException {
-    private final Errors errors;
+    private final List<ErrorResponse> errors;
+
     public ValidationException(Errors errors) {
+        super("validation error", HttpStatus.BAD_REQUEST.value());
+        this.errors = errors.getFieldErrors().stream()
+                .map(error -> new ErrorResponse(error.getDefaultMessage(), error.getField()))
+                .collect(Collectors.toList());
+    }
+
+    public ValidationException(List<ErrorResponse> errors) {
         super("validation error", HttpStatus.BAD_REQUEST.value());
         this.errors = errors;
     }
 
     @Override
     public List<ErrorResponse> serializeErrors() {
-        return errors.getFieldErrors().stream()
-                .map(error -> new ErrorResponse(error.getDefaultMessage(), error.getField()))
-                .collect(Collectors.toList());
+        return errors;
     }
 }
