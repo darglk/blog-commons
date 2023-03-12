57a847091e879141a41c801bfb7a1ef0b16cb962
darglk <darglk@gmail.com>
2023-03-12 13:58:46 +0100

    added field length validation

--- src/main/java/com/darglk/blogcommons/model/LoginRequest.java
+++ src/main/java/com/darglk/blogcommons/model/LoginRequest.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotBlank;
 
+import javax.validation.constraints.Size;
+
 @Data
 public class LoginRequest {
     @Email
     @NotBlank
+    @Size(max = 100)
     private String email;
     @NotBlank
+    @Size(min = 4, max = 100)
     private String password;
 }
